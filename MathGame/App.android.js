e1bc449ad4ad7974c820a4dd54bf2b39dcd19c54
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-08-10 15:31:29 -0500

    init added

---
+++ MathGame/App.android.js
@@ -0,0 +1,22 @@
+// import { bootstrapAnalyticsAsync } from 'expo-cli';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Math Game</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 300,
+  },
+});
